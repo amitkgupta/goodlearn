ea406aafbef369c49be98a44f3117b4430c6afb1
Amit Kumar Gupta <amitkgupta84@gmail.com>
2014-09-08 23:18:27 -0700

    Compute rows eagerly for in-memory dataset for performance

--- data/dataset/dataset.go
+++ data/dataset/dataset.go
@@ -30,7 +30,7 @@ type inMemoryDataset struct {
 	numFeatures          int
 	numTargets           int
 	numColumns           int
-	rawDataset           []float64
+	rows                 []row.Row
 }
 
 func NewDataset(featureColumnIndices, targetColumnIndices []int, columnTypes []columntype.ColumnType) Dataset {
@@ -59,7 +59,7 @@ func NewDataset(featureColumnIndices, targetColumnIndices []int, columnTypes []c
 		len(featureColumnIndices),
 		len(targetColumnIndices),
 		len(columnTypes),
-		[]float64{},
+		[]row.Row{},
 	}
 }
 
@@ -98,28 +98,6 @@ func (dataset *inMemoryDataset) AddRowFromStrings(strings []string) error {
 		rawValues[i] = value
 	}
 
-	dataset.rawDataset = append(dataset.rawDataset, rawValues...)
-
-	return nil
-}
-
-func (dataset *inMemoryDataset) NumRows() int {
-	return len(dataset.rawDataset) / dataset.numColumns
-}
-
-func (dataset *inMemoryDataset) Row(i int) (row.Row, error) {
-	numRows := dataset.NumRows()
-	if i < 0 || numRows <= i {
-		return nil, newDatasetRowIndexOutOfBoundsError(i, numRows)
-	}
-
-	var rawValues []float64
-	if i == numRows-1 {
-		rawValues = dataset.rawDataset[i*dataset.numColumns:]
-	} else {
-		rawValues = dataset.rawDataset[i*dataset.numColumns : (i+1)*dataset.numColumns]
-	}
-
 	features, _ := slice.SliceFromRawValues(
 		dataset.allFeaturesFloats,
 		dataset.featureColumnIndices,
@@ -134,7 +112,22 @@ func (dataset *inMemoryDataset) Row(i int) (row.Row, error) {
 		rawValues,
 	)
 
-	return row.NewRow(features, target, dataset.numFeatures), nil
+	dataset.rows = append(dataset.rows, row.NewRow(features, target, dataset.numFeatures))
+
+	return nil
+}
+
+func (dataset *inMemoryDataset) NumRows() int {
+	return len(dataset.rows)
+}
+
+func (dataset *inMemoryDataset) Row(i int) (row.Row, error) {
+	numRows := dataset.NumRows()
+	if i < 0 || numRows <= i {
+		return nil, newDatasetRowIndexOutOfBoundsError(i, numRows)
+	}
+
+	return dataset.rows[i], nil
 }
 
 func newRowLengthMismatchError(actual, expected int) error {
