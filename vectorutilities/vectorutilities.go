ed383cec1372af813152a1212ce186a01a5b42f1
Amit Kumar Gupta <amitkgupta84@gmail.com>
2014-09-08 23:18:35 -0700

    go fmt ./...

--- vectorutilities/vectorutilities.go
+++ vectorutilities/vectorutilities.go
@@ -1,17 +1,17 @@
 package vectorutilities
 
 func Add(x, y []float64) []float64 {
-    r := make([]float64, len(x))
-    for i := range r {
-        r[i] = x[i] + y[i]
-    }
-    return r
+	r := make([]float64, len(x))
+	for i := range r {
+		r[i] = x[i] + y[i]
+	}
+	return r
 }
 
 func Scale(a float64, x []float64) []float64 {
-    r := make([]float64, len(x))
-    for i := range r {
-        r[i] = a * x[i]
-    }
-    return r
+	r := make([]float64, len(x))
+	for i := range r {
+		r[i] = a * x[i]
+	}
+	return r
 }
