b96337fee94b71c01ff5e8d9a699a599fcfb6aa5
Amit Kumar Gupta <amitkgupta84@gmail.com>
2014-12-29 06:53:04 -0800

    Fix 'NO TESTS' warning

--- matrixparse/matrixparse_suite_test.go
+++ matrixparse/matrixparse_suite_test.go
@@ -1,1 +1,13 @@
 package matrixparse_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func TestMatrixparse(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Matrixparse Suite")
+}
